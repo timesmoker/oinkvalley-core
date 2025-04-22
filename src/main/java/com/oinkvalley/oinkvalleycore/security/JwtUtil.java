b29774fa969bdf368495b3b2552f57332fa13fa0
timesmoker <timesmoker3526@gmail.com>
2025-04-22 11:50:34 +0900

    기본적인 jwt 발급기능

--- src/main/java/com/oinkvalley/oinkvalleycore/security/JwtUtil.java
+++ src/main/java/com/oinkvalley/oinkvalleycore/security/JwtUtil.java
@@ -1,28 +1,38 @@
 package com.oinkvalley.oinkvalleycore.security;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
-
+import jakarta.annotation.PostConstruct;
 import java.security.Key;
 import java.util.Date;
 
+
 @Component
 public class JwtUtil {
+
     @Value("${jwt.secret}")
-    private String secretKey;
-    private Key key;
-    @Value("{jwt.expiration}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
     private Long expiration;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        // 시크릿 키를 byte[]로 변환해서 Key 생성
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
 
     public String generateToken(String userId) {
         return Jwts.builder()
                 .setSubject(userId)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(SignatureAlgorithm.HS256)
+                .signWith(key, SignatureAlgorithm.HS256) // ✅ Key와 알고리즘 명시
                 .compact();
     }
-
 }
