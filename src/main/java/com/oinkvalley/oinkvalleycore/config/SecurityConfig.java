b7e7d7ae95e89a69588ef0820f4693f1736077f1
timesmoker <timesmoker3526@gmail.com>
2025-05-05 22:49:23 +0900

    fix : securityfilterchain 경로에 /api추가

--- src/main/java/com/oinkvalley/oinkvalleycore/config/SecurityConfig.java
+++ src/main/java/com/oinkvalley/oinkvalleycore/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.oinkvalley.oinkvalleycore.config;
 
-import com.oinkvalley.oinkvalleycore.db.domain.User;
-import com.oinkvalley.oinkvalleycore.db.repository.PostRepository;
 import com.oinkvalley.oinkvalleycore.db.repository.UserRepository;
 import com.oinkvalley.oinkvalleycore.security.JwtAuthenticationFilter;
 import com.oinkvalley.oinkvalleycore.security.JwtUtil;
@@ -9,11 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.stereotype.Component;
 
 @EnableMethodSecurity
 @Configuration
@@ -30,10 +28,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/auth/**").permitAll()  // 로그인, 회원가입은 열어두고
-                        .requestMatchers("/boards/**").permitAll() // 게시판 전체 공개
+                        .requestMatchers("/api/auth/**").permitAll()  // 로그인, 회원가입은 열어두고
+                        .requestMatchers("/api/boards/**").permitAll() // 게시판 전체 공개
                         .anyRequest().authenticated()  // 그 외는 인증 필요!
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtUtil, userRepository), UsernamePasswordAuthenticationFilter.class)  // 🔥 JWT 필터 추가!
